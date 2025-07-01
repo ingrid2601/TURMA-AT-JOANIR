8a166536b9932443519f384c5626783da76b691f
ingrid2601 <159648748+ingrid2601@users.noreply.github.com>
2025-07-01 16:35:07 -0300

    .

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function inserirNome(){
 
 inserirNome();
 
-let linguagens = ["JavaScript", "Python", "C++"]
+let linguagens = ["JavaScript", "Python", "C++"];
 console.log (linguagens [0]);
 console.log (linguagens [1]);
 console.log (linguagens [2]);
