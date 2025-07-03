b328da2b9f7b194c7e55346da92ad9ea446bb172
ingrid2601 <159648748+ingrid2601@users.noreply.github.com>
2025-07-03 09:01:21 -0300

    .

--- script.js
+++ script.js
@@ -21,5 +21,5 @@ lista.textContent = linguagens [2];
 
 
 const item = document.querySelector("#lista");
-item.textContent = linguagens [0]+"-" +linguagens [1] + "-"+ linguagens [2]
+item.textContent = linguagens [0] +"-" + linguagens [1] +"-"+ linguagens [2]
 
